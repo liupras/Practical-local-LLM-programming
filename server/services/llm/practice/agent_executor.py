e63b70d495b54c6f41e7d6cad30b07a89aae22c2
liulj <56008507@qq.com>
2025-01-13 20:53:21 +0800

    add agent

--- server/services/llm/practice/agent_executor.py
+++ server/services/llm/practice/agent_executor.py
@@ -24,24 +24,37 @@ def get_wheather_info(
         city_name: Name of the city.        
     """
     print(f'Getting weather information for:{city_name}')
-    return f"{city_name}'s wheather is 15 degrees."
-
-# test
-print(f'get_wheather_info schema:{get_wheather_info.get_input_schema().model_json_schema()}')
-print(f'get_wheather_info tool_call_schema:{get_wheather_info.tool_call_schema.model_json_schema()}')
-print(f'invoke get_wheather_info test:{get_wheather_info.invoke({"city_name": "Beijing"})}')
+    if not city_name:
+        return "city_name parameter is missing. Cannot retrieve weather information."        
+        """
+        **这个返回很重要**
+        返回错误后，agent会放弃这个结果，用自己的能力回答问题，这样结果更加合理；
+        否则，agent会使用空的city_name调用这个tool，并且会拼凑出new york的天气或者别的天气方面的信息。
+        """
+    else:
+        return f"{city_name}'s wheather is 15 degrees."
 
 from langchain_ollama import ChatOllama
 llm = ChatOllama(model="llama3.1",temperature=0,verbose=True)
 
-tools = [
-    get_wheather_info,
-]
-llm_with_tools = llm.bind_tools(tools)
+from langchain_core.messages import HumanMessage
+def test_llm():
+    response = llm.invoke([HumanMessage(content="hi!")])
+    print(f'say hi:{response.content}')
 
-query = 'what is the weather in Beijing?'
-# ai_msg = llm_with_tools.invoke(query)
-#print(f'get_wheather_info tool_calls:{ai_msg.tool_calls}')
+def test_get_wheather_info():
+    print(f'get_wheather_info schema:{get_wheather_info.get_input_schema().model_json_schema()}')
+    print(f'get_wheather_info tool_call_schema:{get_wheather_info.tool_call_schema.model_json_schema()}')
+    print(f'invoke get_wheather_info test:{get_wheather_info.invoke({"city_name": "Beijing"})}')
+
+    tools = [
+        get_wheather_info,
+    ]
+    llm_with_tools = llm.bind_tools(tools)
+
+    query = 'what is the weather in Beijing?'
+    ai_msg = llm_with_tools.invoke(query)
+    print(f'get_wheather_info tool_calls:{ai_msg.tool_calls}')
 
 # Retrieve
 
@@ -51,98 +64,117 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_ollama import OllamaEmbeddings
-embedding_model = OllamaEmbeddings(model="nomic-embed-text")
 
-loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
-docs = loader.load()
-documents = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200
-).split_documents(docs)
-vector = Chroma.from_documents(documents, embedding_model)
-retriever = vector.as_retriever()
+def get_retriever():
+    embedding_model = OllamaEmbeddings(model="nomic-embed-text")
+
+    loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
+    docs = loader.load()
+    documents = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200
+    ).split_documents(docs)
+    vector = Chroma.from_documents(documents, embedding_model)
+    retriever = vector.as_retriever()
+    return retriever
 
 #print(f'retriever.invoke:{retriever.invoke("how to upload a dataset")}')
 
 from langchain.tools.retriever import create_retriever_tool
 
 retriever_tool = create_retriever_tool(
-    retriever,
+    get_retriever(),
     "langsmith_search",
     "Use this tool only if you are searching for information about LangSmith!",
 )
 
-
-# LLM Test
-from langchain_core.messages import HumanMessage
-
-#response = llm.invoke([HumanMessage(content="hi!")])
-#print(f'say hi:{response.content}')
-
-# tools test
 tools = [get_wheather_info, retriever_tool]
 model_with_tools = llm.bind_tools(tools)
 
-"""
-response = model_with_tools.invoke([HumanMessage(content='Hi!)])
+def test_tools():
 
-# 返回结果不理想：ContentString为空，却有ToolCalls
-print(f"ContentString: {response.content}")
-print(f"ToolCalls: {response.tool_calls}")
+    response = model_with_tools.invoke([HumanMessage(content='Hi!')])
 
+    # 返回结果不理想：ContentString为空，却有ToolCalls
+    print(f"ContentString: {response.content}")
+    print(f"ToolCalls: {response.tool_calls}")
 
-response = model_with_tools.invoke([HumanMessage(content="What's the weather in SF?")])
+    response = model_with_tools.invoke([HumanMessage(content="What's the weather in SF?")])
 
-print(f"ContentString: {response.content}")
-print(f"ToolCalls: {response.tool_calls}")
-"""
+    print(f"ContentString: {response.content}")
+    print(f"ToolCalls: {response.tool_calls}")
+
 
 # Create the agent
 
-from langchain import hub
 from langchain_core.prompts import ChatPromptTemplate
-
-# hwchase17/openai-functions-agent 这个提示词在llama3上不准确：发送简单的问候词 hi 时，也会调用 get_wheather_info
-"""
-prompt = hub.pull("hwchase17/openai-functions-agent")
-print(f'openai-functions-agent:{prompt}')
-prompt_openai = ChatPromptTemplate([
-    ("system", "You are a helpful assistant."),
-    ("placeholder", "{chat_history}"),
-    ("human", "{input}"),
-    ("placeholder", "{agent_scratchpad}"),
-])
-print(f'prompt_openai-functions-agent:{prompt_openai}')
-"""
-
-# ily294/llama-structured-functions
-#prompt_llama = hub.pull("ily294/llama-functioncalling-v3")
-systemprompt = """You are a helpful assistant with access to the following functions. Use them if required:
-
-[get_wheather_info,langsmith_search]
-
-If one of the parameters for the function is empty, do not call this function and directly use the capabilities of the large language model itself to return the answer.
-"""
-prompt = ChatPromptTemplate([
-    ("system", "You are a helpful assistant with access to the following functions:[get_wheather_info,langsmith_search]. **Use them if required**!"),
-    ("placeholder", "{chat_history}"),
-    ("human", "{input}"),
-    ("placeholder", "{agent_scratchpad}"),
-])
-
-
 from langchain.agents import create_tool_calling_agent
 
-agent = create_tool_calling_agent(llm, tools, prompt)
+def get_agent():
+    
+    # 此prompt是基于hwchase17/openai-functions-agent修改的
+    systemprompt = """You are a helpful assistant with access to the following functions:
+
+    [get_wheather_info,langsmith_search]. 
+
+    - **Use them only if required**!"""
+    prompt = ChatPromptTemplate([
+        ("system", systemprompt),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ])
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    return agent
 
 from langchain.agents import AgentExecutor
+agent_executor = AgentExecutor(agent=get_agent(), tools=tools)
 
-agent_executor = AgentExecutor(agent=agent, tools=tools)
+def test_agent_executor():
+    r = agent_executor.invoke({"input": "hi!"})
+    print(f'agent_executor.invoke 1:\n{r}')
 
-# Run the agent
-r = agent_executor.invoke({"input": "hi!"})
-print(f'agent_executor.invoke:{r}')
+    r = agent_executor.invoke({"input": "what is the weather in sf?"})
+    print(f'agent_executor.invoke 2:\n{r}')
 
-r = agent_executor.invoke({"input": "whats the weather in sf?"})
-print(f'agent_executor.invoke:{r}')
+    r = agent_executor.invoke({"input": "how to start LangSmith?"})
+    print(f'agent_executor.invoke 3:\n{r}')
 
 
+# Adding in memory
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+def test_agent_with_chat_history():
+    session_id = "liupras"
+    r = agent_with_chat_history.invoke(
+        {"input": "hi! I'm bob"},
+        config={"configurable": {"session_id": session_id}},
+    )
+    print(f'agent_with_chat_history.invoke 1:\n{r}')
+
+    r = agent_with_chat_history.invoke(
+        {"input": "what's my name?"},
+        config={"configurable": {"session_id": session_id}},
+    )
+    print(f'agent_with_chat_history.invoke 2:\n{r}')
+
+if __name__ == '__main__':
+    test_agent_with_chat_history()
