771c2d4a7997e74196a0b8f1dc0a0fbdbe874110
liulj <56008507@qq.com>
2025-01-27 19:22:07 +0800

    add

--- server/services/practice/04_classification.py
+++ server/services/practice/04_classification.py
@@ -6,13 +6,15 @@
 # @version : V0.5
 # @Description ：给文本打标签/分类。
 
-# https://python.langchain.com/docs/tutorials/classification/
+# 参考：https://python.langchain.com/docs/tutorials/classification/
+
+from langchain_ollama import ChatOllama
+llm = ChatOllama(model="llama3.1",temperature=0.2,verbose=True)
 
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
-from langchain_ollama import ChatOllama
 
-def simple_control():
+def simple_control(s):
 
     tagging_prompt = ChatPromptTemplate.from_template(
         """
@@ -25,6 +27,7 @@ def simple_control():
     """
     )
 
+    # 指定​​ Pydantic 模型控制返回内容格式
     class Classification(BaseModel):
         sentiment: str = Field(description="The sentiment of the text")
         aggressiveness: int = Field(
@@ -33,32 +36,19 @@ def simple_control():
         language: str = Field(description="The language the text is written in")
 
 
+    llm_structured = llm.with_structured_output(Classification)
+    prompt = tagging_prompt.invoke({"input": s})
+    response = llm_structured.invoke(prompt)
 
-    llm = ChatOllama(model="llama3.1",temperature=0,verbose=True).with_structured_output(
-        Classification
-    )
+    return response.model_dump()
 
-    inp = "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!"
-    prompt = tagging_prompt.invoke({"input": inp})
-    response = llm.invoke(prompt)
-
-    print(f'response:\n{response}') 
-
-    # dictionary output
-    inp = "Estoy muy enojado con vos! Te voy a dar tu merecido!"
-    prompt = tagging_prompt.invoke({"input": inp})
-    response = llm.invoke(prompt)
-
-    print(f'response:\n{response.model_dump()}') 
-
-
-def finer_control():    
+def finer_control(s):    
     """
     官网使用OpenAI，我们使用的是本地大模型。
     直接用官网的代码效果不好：sentiment无法按预期标记出happy,neutral,sad，依然只能标记出：positive、negative；aggressiveness的值一直为0。
-    mistral貌似比llama3.1更好一点，能更准确的进行标记。
     """
 
+    # 指定​​ Pydantic 模型控制返回内容格式
     class Classification(BaseModel):
         sentiment: str = Field(description="The sentiment of the text,it must be one of happy,neutral,sad")
         aggressiveness: int = Field(description="The aggressive of the text,it must be one of 1,2,3,4,5,6,7,8,9,10,the higher the number the more aggressive")
@@ -77,24 +67,31 @@ def finer_control():
     )
 
 
-    llm = ChatOllama(model="llama3.1",temperature=0.5,verbose=True).with_structured_output(
-        Classification
-    )
+    llm_structured = llm.with_structured_output(Classification)
 
-    inp = "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!"
-    prompt = tagging_prompt.invoke({"input": inp})
-    response = llm.invoke(prompt)
-    print(f'response:\n{response.model_dump()}') 
+    prompt = tagging_prompt.invoke({"input": s})
+    response = llm_structured.invoke(prompt)
+    return response.model_dump()
 
-    inp = "Weather is ok here, I can go outside without much more than a coat"
-    prompt = tagging_prompt.invoke({"input": inp})
-    response =llm.invoke(prompt)
-    print(f'response:\n{response}') 
-
-    inp = "今天的天气糟透了，我什么都不想干！"
-    prompt = tagging_prompt.invoke({"input": inp})
-    response =llm.invoke(prompt)
-    print(f'response:\n{response}') 
 
 if __name__ == '__main__':
-    finer_control()
+    
+    s = "I'm incredibly glad I met you! I think we'll be great friends!"
+    result = simple_control(s)
+    print(f'result:\n{result}')
+    
+    s = "Estoy muy enojado con vos! Te voy a dar tu merecido!"
+    result = simple_control(s)
+    print(f'result:\n{result}')
+    
+    s = "I'm incredibly glad I met you! I think we'll be great friends!"
+    result = finer_control(s)
+    print(f'finer_control result:\n{result}')
+    
+    s = "Weather is ok here, I can go outside without much more than a coat"
+    result = finer_control(s)
+    print(f'finer_control result:\n{result}')
+
+    s="今天的天气糟透了，我什么都不想干！"
+    result = finer_control(s)
+    print(f'finer_control result:\n{result}')
